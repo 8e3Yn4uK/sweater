8127da5afb518b0bd32d35d897cfe41c45985be1
8e3yn4uk <sashko.kondratenko@gmail.com>
2019-08-31 20:47:52 +0300

    init

---
+++ src/main/java/com/ve3yn4uk/sweater/SweaterApplication.java
@@ -0,0 +1,13 @@
+package com.ve3yn4uk.sweater;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SweaterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SweaterApplication.class, args);
+    }
+
+}
